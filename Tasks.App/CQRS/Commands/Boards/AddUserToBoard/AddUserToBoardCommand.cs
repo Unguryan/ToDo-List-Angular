e323e6606430e64523981c24ee1d1e99146f5fee
Aleksandr Unhurian <unguryan.sasha@gmail.com>
2023-04-12 15:21:48 +0300

    + new command

--- Tasks.App/CQRS/Commands/Boards/AddUserToBoard/AddUserToBoardCommand.cs
+++ Tasks.App/CQRS/Commands/Boards/AddUserToBoard/AddUserToBoardCommand.cs
@@ -2,5 +2,5 @@ using MediatR;
 
 namespace Tasks.App.CQRS.Commands.Boards.AddUserToBoard
 {
-    public record AddUserToBoardCommand() : IRequest<AddUserToBoardCommandResult>;
+    public record AddUserToBoardCommand(int BoardId, int UserId, int UserIdToAdd) : IRequest<AddUserToBoardCommandResult>;
 }
